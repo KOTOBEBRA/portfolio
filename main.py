554c0ad00f25237baf73d794e91097b83624aa3a
KOTOBEBRA <98683286+KOTOBEBRA@users.noreply.github.com>
2022-01-30 11:18:23 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+contact_information = {
+    'Имя': 'Андрей',
+    'Фамилия': 'Иванов',
+    'Возраст': 13,
+    'КлASS': 7,
+    'Номер телефона': '+7 962 321-19-50'
+    }
+
+path = 'static/bongocat.jpg' # путь до картинок
+
+
+@app.route('/')
+def index():
+    return render_template(
+        'index.html',
+        name=contact_information['Имя'],
+        data=contact_information,
+        photo=path
+    )
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+    
